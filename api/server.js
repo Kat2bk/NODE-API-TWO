4b453af3e2637f7f00814e4ccefd0bf2d3770ec1
unknown <Katherine2bk@gmail.com>
2021-06-10 17:23:51 -0700

    set up server on index.js, set up main server.get in server.js... everything works

--- api/server.js
+++ api/server.js
@@ -1,2 +1,12 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require("express");
+//postsRouter
+const server = express();
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.json({ message: "Welcome to the API for posts" });
+});
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// require your server and launch it here
+const server = require("./api/server");
+
+const port = 8080;
+
+server.listen(port, () => {
+  console.log("\n --- server is listening on port 8080 ---");
+});
