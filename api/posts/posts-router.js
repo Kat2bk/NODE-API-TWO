84f8f5cba7c95d43f8ee75d8a7e7caa925ceb21b
unknown <Katherine2bk@gmail.com>
2021-06-10 18:39:17 -0700

    run tests: 13 passed out of 17, 4 failed

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -16,9 +16,7 @@ router.get("/", async (req, res) => {
     const posts = await data.find();
     res.status(200).json(posts);
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: "The posts information could not be retrieved" });
+    res.status(500).json({ message: "The posts information could not be retrieved" });
   }
 });
 
@@ -36,12 +34,10 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     const post = await data.findById(req.params.id);
-    if (!req.params.id) {
-      res
-        .status(404)
-        .json({ message: "The post with the specified ID does not exist" });
+    if (post) {
+        res.status(200).json(post)
     } else {
-      res.status(200).json(post);
+        res.status(404).json({ message: "The post with the specified ID does not exist" });
     }
   } catch (error) {
     res.status(500).json({ message: "The post info could not be retrieved" });
@@ -151,12 +147,10 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   try {
     const deleted = await data.remove(req.params.id);
-    if (!req.params.id) {
-      res
-        .status(404)
-        .json({ message: "The post with the specified ID does not exist" });
+    if (deleted) {
+        res.status(204).json(deleted)
     } else {
-      res.status(204).json(deleted);
+        res.status(404).json({message: "The post with the specified ID does not exist"})
     }
   } catch (error) {
     res.status(500).json({ message: "The post could not be removed" });
@@ -181,14 +175,10 @@ router.get("/:id/comments", async (req, res) => {
     if (comments) {
       res.status(200).json(comments);
     } else {
-      res
-        .status(404)
-        .json({ message: "The post with the specified ID does not exist" });
+      res.status(404).json({ message: "The post with the specified ID does not exist" });
     }
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: "The comments info could not be retrieved" });
+    res.status(500).json({ message: "The comments info could not be retrieved" });
   }
 });
 
