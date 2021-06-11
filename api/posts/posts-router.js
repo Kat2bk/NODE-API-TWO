97ecff26c099d61e8b916dd895ca118791b0d1b0
unknown <Katherine2bk@gmail.com>
2021-06-10 18:22:43 -0700

    router.get commentsById works, everything is finished... beginning tests

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -175,6 +175,21 @@ router.delete("/:id", async (req, res) => {
 //   - respond with HTTP status code `500`.
 //   - return the following JSON: `{ message: "The comments information could not be retrieved" }`.
 
-router.get("/:id/comments", async (req, res) => {});
+router.get("/:id/comments", async (req, res) => {
+  try {
+    const comments = await data.findCommentById(req.params.id);
+    if (comments) {
+      res.status(200).json(comments);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "The comments info could not be retrieved" });
+  }
+});
 
 module.exports = router;
