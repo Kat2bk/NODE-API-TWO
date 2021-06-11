74934c11785d77bf40b29ab3a82e2a0c91e2888c
unknown <Katherine2bk@gmail.com>
2021-06-10 17:29:39 -0700

    set up postsRouter inside server.js...

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 // implement your server here
 // require your posts router and connect it here
 const express = require("express");
-//postsRouter
+const postsRouter = require("./posts/posts-router");
 const server = express();
 server.use(express.json());
+
+server.use("/api/posts", postsRouter);
 
 server.get("/", (req, res) => {
   res.json({ message: "Welcome to the API for posts" });
